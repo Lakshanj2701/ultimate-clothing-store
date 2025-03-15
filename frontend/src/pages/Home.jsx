994cc4ddd632b0fc47d36fd949c04fb216dc39da
Lakshan <lahirulakshan2701@gmail.com>
2025-03-15 22:28:24 +0530

    up to best seller

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../components/Layout/Hero';
 import GenderCollectionSection from '../components/Products/GenderCollectoinSection';
 import NewArrivals from '../components/Products/NewArrivals';
+import ProductDetails from '../components/Products/ProductDetails';
 
 const Home = () => {
   return (
@@ -9,6 +10,12 @@ const Home = () => {
         <Hero/>
         <GenderCollectionSection/>
         <NewArrivals/>
+
+        {/* best seller sectoin*/}
+        <h2 className="text-3xl text-center font-bold mb-4">
+            Best seller
+        </h2>
+        <ProductDetails/>
     </div>
   );
 };
