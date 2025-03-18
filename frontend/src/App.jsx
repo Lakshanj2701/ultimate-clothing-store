b322eff26bb875d9f2f773bf234baba511db1949
Upeksha Gimhani <167627109+UpekshaGimhani@users.noreply.github.com>
2025-03-18 20:01:25 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserLayout from './components/Layout/UserLayout';
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
-import AdminLayout from './components/Admin/AdminLayout';
-import AdminHomePage from './pages/AdminHomePage';
-import UserManagement from './components/Admin/UserManagement';
-import ProductManagement from './components/Admin/ProductManagement';
-import EditProductPage from './components/Admin/EditProductPage';
-import OrderManagement from './components/Admin/OrderManagement';
 
 const App = () => {
   return (
@@ -19,13 +13,7 @@ const App = () => {
         <Route path="/" element={<UserLayout/>}> 
          <Route index element={<Home />} />
         </Route>
-        <Route path="/admin" element={< AdminLayout/>}>
-          <Route index element={<AdminHomePage/>}/>
-          <Route path="users" element={<UserManagement/>}/>
-          <Route path="products" element={<ProductManagement/>}/>
-          <Route path="products/:id/edit" element={<EditProductPage/>}/>
-          <Route path="orders" element={<OrderManagement/>}/>
-        </Route>
+        <Route>{/* Admin Layout*/}</Route>
 
       </Routes>
     </BrowserRouter>
