c0acaf1982f3eab2819086ec6ab21921bc573082
Chathushka Hasaranga <chathushka03hasaranga@gmail.com>
2025-03-19 16:14:49 +0530

    userLayout done

--- frontend/src/components/Cart/PayPalButton.jsx
+++ frontend/src/components/Cart/PayPalButton.jsx
@@ -3,7 +3,9 @@ import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 const PayPalButton = ({ amount, onSuccess, onError }) => {
   return (
-    <PayPalScriptProvider options={{ "client-id": "ASLo0PMZvjYqE-zvgMODqYnDlbuTwB6iR-LOidwKfX71OqnPuY2OoQr7VDZlRsxf14qspfvK6WlNB6Rd" }}>
+    <PayPalScriptProvider options={{ "client-id": 
+    import.meta.env.VITE_PAYPAL_CLIENT_ID, 
+    }}>
       <PayPalButtons
         style={{ layout: "vertical" }}
         createOrder={(data, actions) => {
