ec2eeb62a0b86cc020c36a8dd7273e684ca5b19c
Lakshan <lahirulakshan2701@gmail.com>
2025-03-21 20:18:48 +0530

    product management up to update

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -24,5 +24,17 @@ const protect = async (req, res, next) => {
   }
 };
 
+
+const admin = (req, res, next) => {
+    if (req.user && req.user.role === "admin") {
+      next();
+    } else {
+      res.status(403).json({ message: "Not authorized as an admin" });
+    }
+  };
+  
+  module.exports = admin;
+  
+
 // Correct export
-module.exports = { protect };
+module.exports = { protect, admin };
