ca83f4e28fd4b67e3d8c36654ee61c5458290c2d
Chathushka Hasaranga <chathushka03hasaranga@gmail.com>
2025-03-24 15:14:12 +0530

    Order Done

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
-
+const checkOutRoutes = require("./routes/checkOutRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
 const app = express(); 
 
@@ -30,6 +31,9 @@ app.get("/", (req, res) => {
 app.use("/api/users",userRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/cart",cartRoutes);
+app.use("/api/checkout", checkOutRoutes);
+app.use("/api/orders", orderRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`); 
