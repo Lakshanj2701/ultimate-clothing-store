d7671c258358bd32b2726a7352dc3ca64eaf28d7
Rukshan <rukshantharusha85@gmail.com>
2025-03-26 22:19:21 +0530

    advertisementDone

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,11 @@ const app = express();
 
 dotenv.config();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 const PORT = process.env.PORT || 9000;
 
