447147689963b815125b19d72f26fea5c7fc97f8
Lakshan <lahirulakshan2701@gmail.com>
2025-03-27 09:43:14 +0530

    done

--- frontend/src/components/Products/GenderCollectoinSection.jsx
+++ frontend/src/components/Products/GenderCollectoinSection.jsx
@@ -25,7 +25,7 @@ const GenderCollectionSection = () => {
                 <img src={mensCollectionImage} alt="Men's Collection" className="w-full h-[700px] object-cover"/>
                 <div className="absolute bottom-8 left-8 bg-white bg-opacity-90 p-4">
                     <h2 className="text-2xl font-bold text-gray-900 mb-3">
-                    Women's Collection
+                    Men's Collection
                     </h2>
                     <Link to="/collections/all?gender-Men" className="text-gray-900 underline">
                         Shop Now
